ce87b3ab29fc7f89948296aa2afddb5cf25793db
nnaeyno <ngogo20@freeuni.edu.ge>
2024-12-27 14:40:42 +0400

    admin

--- city/admin.py
+++ city/admin.py
@@ -1,3 +1,127 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from .models import Location, City, Place, PlaceRating, PlaceComment, UserPlaceVisit
 
-# Register your models here.
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'lat', 'lng', 'get_places')
+    search_fields = ('lat', 'lng')
+
+    def get_places(self, obj):
+        return ", ".join([place.name for place in obj.places.all()])
+
+    get_places.short_description = 'Associated Places'
+
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'get_places_count')
+    search_fields = ('name',)
+
+    def get_places_count(self, obj):
+        return obj.places.count()
+
+    get_places_count.short_description = 'Number of Places'
+
+
+class PlaceRatingInline(admin.TabularInline):
+    model = PlaceRating
+    extra = 0
+    readonly_fields = ('created_at',)
+    raw_id_fields = ('user',)
+
+
+class PlaceCommentInline(admin.TabularInline):
+    model = PlaceComment
+    extra = 0
+    readonly_fields = ('created_at', 'updated_at')
+    raw_id_fields = ('user',)
+
+
+class UserPlaceVisitInline(admin.TabularInline):
+    model = UserPlaceVisit
+    extra = 0
+    readonly_fields = ('visited_at',)
+    raw_id_fields = ('user',)
+
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'city', 'price', 'average_rating', 'total_ratings',
+                    'display_photo', 'created_at')
+    list_filter = ('city', 'created_at')
+    search_fields = ('name', 'description', 'city__name')
+    readonly_fields = ('created_at', 'updated_at', 'total_ratings', 'average_rating',
+                       'display_photo_large')
+    raw_id_fields = ('location',)
+    inlines = [PlaceRatingInline, PlaceCommentInline, UserPlaceVisitInline]
+    fieldsets = (
+        ('Basic Information', {
+            'fields': ('name', 'description', 'city', 'price')
+        }),
+        ('Location', {
+            'fields': ('location',)
+        }),
+        ('Media', {
+            'fields': ('photo', 'display_photo_large')
+        }),
+        ('Statistics', {
+            'fields': ('total_ratings', 'average_rating')
+        }),
+        ('Metadata', {
+            'fields': ('created_at', 'updated_at'),
+            'classes': ('collapse',)
+        })
+    )
+
+    def display_photo(self, obj):
+        if obj.photo:
+            return format_html('<img src="{}" height="50"/>', obj.photo.url)
+        return "No photo"
+
+    display_photo.short_description = 'Photo'
+
+    def display_photo_large(self, obj):
+        if obj.photo:
+            return format_html('<img src="{}" height="200"/>', obj.photo.url)
+        return "No photo"
+
+    display_photo_large.short_description = 'Photo Preview'
+
+
+@admin.register(PlaceRating)
+class PlaceRatingAdmin(admin.ModelAdmin):
+    list_display = ('user', 'place', 'rating', 'created_at')
+    list_filter = ('rating', 'created_at')
+    search_fields = ('user__username', 'place__name')
+    raw_id_fields = ('user', 'place')
+    readonly_fields = ('created_at',)
+
+
+@admin.register(PlaceComment)
+class PlaceCommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'place', 'text_preview', 'created_at', 'updated_at')
+    list_filter = ('created_at', 'updated_at')
+    search_fields = ('user__username', 'place__name', 'text')
+    raw_id_fields = ('user', 'place')
+    readonly_fields = ('created_at', 'updated_at')
+
+    def text_preview(self, obj):
+        return obj.text[:50] + '...' if len(obj.text) > 50 else obj.text
+
+    text_preview.short_description = 'Comment Preview'
+
+
+@admin.register(UserPlaceVisit)
+class UserPlaceVisitAdmin(admin.ModelAdmin):
+    list_display = ('user', 'place', 'visited_at', 'notes_preview')
+    list_filter = ('visited_at',)
+    search_fields = ('user__username', 'place__name', 'notes')
+    raw_id_fields = ('user', 'place')
+    readonly_fields = ('visited_at',)
+
+    def notes_preview(self, obj):
+        return obj.notes[:50] + '...' if len(obj.notes) > 50 else obj.notes
+
+    notes_preview.short_description = 'Notes Preview'
--- user/admin.py
+++ user/admin.py
@@ -1,5 +1,96 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.html import format_html
+from django.utils.translation import gettext_lazy as _
+from .models import User, Notification
 
-from user.models import User
 
-admin.site.register(User)
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ('username', 'email', 'status', 'display_photo', 'created_at',
+                    'is_active', 'is_staff')
+    list_filter = ('status', 'is_active', 'is_staff', 'created_at')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    ordering = ('-created_at',)
+    filter_horizontal = ('cities', 'groups', 'user_permissions')
+    readonly_fields = ('created_at', 'display_photo_large')
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {
+            'fields': ('username', 'first_name', 'last_name', 'profile_photo',
+                       'display_photo_large')
+        }),
+        (_('Locations'), {
+            'fields': ('cities',)
+        }),
+        (_('Status'), {
+            'fields': ('status',)
+        }),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups',
+                       'user_permissions'),
+        }),
+        (_('Important dates'), {
+            'fields': ('last_login', 'created_at', 'date_joined')
+        }),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'password1', 'password2'),
+        }),
+    )
+
+    def display_photo(self, obj):
+        if obj.profile_photo:
+            return format_html('<img src="{}" height="50"/>', obj.profile_photo.url)
+        return "No photo"
+
+    display_photo.short_description = 'Profile Photo'
+
+    def display_photo_large(self, obj):
+        if obj.profile_photo:
+            return format_html('<img src="{}" height="200"/>', obj.profile_photo.url)
+        return "No photo"
+
+    display_photo_large.short_description = 'Profile Photo Preview'
+
+
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ('recipient', 'sender', 'message_preview', 'is_read', 'created_at')
+    list_filter = ('is_read', 'created_at')
+    search_fields = ('recipient__username', 'recipient__email',
+                     'sender__username', 'sender__email', 'message')
+    raw_id_fields = ('recipient', 'sender')
+    readonly_fields = ('created_at',)
+    actions = ['mark_as_read', 'mark_as_unread']
+
+    def message_preview(self, obj):
+        return obj.message[:50] + '...' if len(obj.message) > 50 else obj.message
+
+    message_preview.short_description = 'Message Preview'
+
+    def mark_as_read(self, request, queryset):
+        queryset.update(is_read=True)
+
+    mark_as_read.short_description = "Mark selected notifications as read"
+
+    def mark_as_unread(self, request, queryset):
+        queryset.update(is_read=False)
+
+    mark_as_unread.short_description = "Mark selected notifications as unread"
+
+    fieldsets = (
+        ('Notification Details', {
+            'fields': ('recipient', 'sender', 'message', 'is_read')
+        }),
+        ('Metadata', {
+            'fields': ('created_at',),
+            'classes': ('collapse',)
+        }),
+    )
+
+
+admin.site.register(Notification, NotificationAdmin)
