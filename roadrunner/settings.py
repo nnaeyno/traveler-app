facb6fc663a7c126d180bf829a4d237be58f50b5
nnaeyno <ngogo20@freeuni.edu.ge>
2024-12-31 14:38:56 +0400

    remove pagination for now

--- roadrunner/settings.py
+++ roadrunner/settings.py
@@ -133,8 +133,6 @@ SESSION_SAVE_EVERY_REQUEST = True
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
